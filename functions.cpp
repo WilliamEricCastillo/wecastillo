12ede3f106ecfdefd27d8bd7eedf27ba6021328f
willcastl3 <willcastl3@outlook.com>
2021-08-27 19:32:14 -0500

    operating string_length function

--- functions.cpp
+++ functions.cpp
@@ -2,5 +2,12 @@
 
 int string_length(const char *str){
 
-    return 0;
+    int count = 0;
+
+    for( int i = 0; i < str[i]; ++i)
+    {
+        count += 1;
+    }
+
+    return count;
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 /*********************
-* Your comment header here.
+Name: William Castillo
+COSC-2436-003
+Purpose: Demonstrate local and remote repository skills
 **********************/
 
 #include "main.h"
@@ -9,8 +11,12 @@ int main(int argc, char** argv) {
     cout << "My Command line analyzer..." << endl;
     cout<< "The number of command line parameters are: "<< argc << endl;
 
-    if(argc > 1){
-        std::cout << "The length of the first parameter is "<< string_length(argv[1]) << std::endl;
+    if(argc >1)
+    {
+        cout<< "The length of the first of the first parameter is: " << string_length(argv[1]) << endl;
     }
+
     return 0;
 }
+
+
