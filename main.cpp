a2fbc76d05392a2fa18a213f1eb020d2d061ae31
willcastl3 <willcastl3@outlook.com>
2021-08-29 16:51:26 -0500

    Removed  if statement in main() causing bug when just the program name is entered

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /*********************
 Name: William Castillo
-COSC-2436-003
+Coding 01
 Purpose: This program is a command line analyzer that displays the number of command line parameters,
 the string length of the program's name, and the string length of each parameter.
 **********************/
@@ -10,16 +10,13 @@ the string length of the program's name, and the string length of each parameter
 int main(int argc, char** argv) {
     cout << "My Command line analyzer..." << endl;
     cout << "The number of command line parameters are: " << argc << endl;
-
-    if(argc > 1)
-    {
-        cout << "The length of the name of the program is: " << string_length(argv[1]) << endl;
+    cout << "The length of the name of the program is: " << string_length(argv[0]) << endl;
 
         for(int i = 1; i < argc; ++i){
 
             cout << "The length of the parameter " << i << " is " << string_length(argv[i]) << endl;
         }
-    }
+
     return 0;
 }
 
