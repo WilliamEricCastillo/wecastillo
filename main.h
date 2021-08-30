e8a7fa5233cf98d03b5a5d81e8f80a656ed966d6
willcastl3 <willcastl3@outlook.com>
2021-08-29 19:55:39 -0500

    added coding 01 to comment header

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 /*********************
 Name: William Castillo
-COSC-2436-003
+Coding 01
 Purpose: This program is a command line analyzer that displays the number of command line parameters,
 the string length of the program's name, and the string length of each parameter.
 **********************/
