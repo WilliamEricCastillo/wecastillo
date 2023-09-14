6bb19bc2041ecf7c0e3d38971c51b97bd3f42d5c
willcastl3 <willcastl3@outlook.com>
2023-09-14 05:51:38 -0500

    Changed homepage text

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <p>
+              William's Personal Website
+          </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          UNDER CONSTRUCTION
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
